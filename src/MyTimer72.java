aa5cad38784f4055b4421f6ce80cfc66b7eed51b
asher alexander <asherb94@gmail.com>
2019-04-13 23:11:29 +0300

    here we start!

---
+++ src/MyTimer72.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author levian evTime
+ *
+ */
+ 
+public class MyTimer72 extends Thread
+{
+    private final long time;
+    private final Event64 evTime;
+
+    public MyTimer72(long time,Event64 evTime)
+    {
+        this.time=time;
+        this.evTime=evTime;
+        setDaemon(true);
+        start();
+    }
+
+    public void run()
+    {
+        try 
+		{
+            sleep(time);
+        } catch (InterruptedException ex) {}
+        evTime.sendEvent();
+    }
+
+}
